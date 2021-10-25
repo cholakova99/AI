ff25cd50e20779ebfe0c3efeb49f920f6756f87b
Zlatina Cholakova <zlatinatc99@gmail.com>
2021-10-25 13:42:00 +0300

    typo

--- bfs_find_path/find_path_in_matrix.py
+++ bfs_find_path/find_path_in_matrix.py
@@ -133,3 +133,4 @@ if __name__ == '__main__':
     solve(starting_row, starting_col, ending_row, ending_col)
     path = deconstruct(starting_row, starting_col, ending_row, ending_col)
     pr_print_matrix(matrix, path)
+    
